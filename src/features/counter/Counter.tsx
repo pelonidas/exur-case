90980e362143379c4f87695b3ef5cb312da09361
pelonidas <janx803y@easv365.dk>
2022-07-12 13:05:33 +0200

    Init commit

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -9,7 +9,6 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../../app/store';
 import { fetchCount } from './counterAPI';
+import axios from "axios";
 
 export interface CounterState {
   value: number;
@@ -18,9 +19,9 @@ const initialState: CounterState = {
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
 export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount: number) => {
-    const response = await fetchCount(amount);
+  'counter/fetchProducts',
+  async (url: string) => {
+    const response = await axios.get(url);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
